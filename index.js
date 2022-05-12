2def0b57b0afe17b7ae599b4722e8e730be2eff0
marck283 <86911602+marck283@users.noreply.github.com>
2022-05-12 16:22:24 +0200

    Uno4

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express();
 
 var port = 3000;
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
     console.log("GET request received in port " + port);
     res.sendFile(".signin.html");
