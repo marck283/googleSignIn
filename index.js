7bebf307920c27ee4bf20b5d2bda40b2188c2413
marck283 <86911602+marck283@users.noreply.github.com>
2022-05-12 16:28:08 +0200

    Added post request middleware

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    console.log("GET request received in port " + port);
-    res.sendFile("./signin.html");
+    console.log("GET request received on port " + port);
+    res.send("./signin.html");
 });
+
+app.post("/", (req, res) => {
+    console.log("POST request received on port " + port);
+    res.send("./signin.html");
+})
 
 http.createServer(app).listen((req, res) => {
     console.log("Server running on port " + port);
