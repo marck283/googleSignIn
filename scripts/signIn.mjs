ba39a9f607a858e001e9b5fb6be2df9aca877191
marck283 <86911602+marck283@users.noreply.github.com>
2022-05-13 00:17:51 +0200

    Uno19

--- scripts/signIn.mjs
+++ scripts/signIn.mjs
@@ -1,18 +1,5 @@
 import * as jose from "jose";
-export function handleCredentialResponse(response) {
-    // decodeJwtResponse() is a custom function defined by you
-    // to decode the credential response.
-    const responsePayload = decodeJwtResponse(response.credential).payload;
-
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
-}
-
-function decodeJwtResponse(credential) {
+export function decodeJwtResponse(credential) {
     const jwt = credential;
     return { payload, protectedHeader } = await jose.jwtVerify(jwt, publicKey, {
         issuer: 'urn:example:issuer',
